2c1adf599e407904024d868c492c104d82da3b8b
Erica Nafziger <garbererica@gmail.com>
2016-10-26 21:46:23 -0700

    add basic functionality to identify words beginning with vowels and words beginning with a consonant; can also handle multiple word strings

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+var pigLatin = function(input) {
+
+  var vowels = ["a", "e", "i", "o", "u"];
+  var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+  var words = input.split(" ");
+  newWord = "";
+
+  words.forEach(function(word){
+    var letterWord = word.toLowerCase();
+    var splitWord = letterWord.split("");
+    alert(splitWord);
+
+    if (splitWord[0]=== "a" || splitWord[0]=== "e" || splitWord[0]=== "i" || splitWord[0]=== "o" || splitWord[0]=== "u") {
+      var joinedWord = splitWord.join("");
+      var newWord = joinedWord + "ay";
+      alert(newWord);
+    } else {
+      var firstLetter = splitWord.shift();
+      splitWord.push(firstLetter);
+      newWord = splitWord.join("")+ "ay";
+      alert(newWord);
+    }
+    return newWord;
+
+});//end of words function
+
+}; //end of pigLatin function
+
+
+
+
+//user interface logic below
+$(document).ready(function(){
+  $("#input").submit(function(event){
+    event.preventDefault();
+    var input = $("#input input").val();
+    var result = pigLatin(input);
+    $("#output").text(result);
+  });
+
+});
